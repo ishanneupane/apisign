abdcd112c2792ae2f91403ded8236b11b78f9d50
Ishan Neupane <ishanneupane8945@gmail.com>
2024-02-02 13:21:43 +0545

    model used

--- lib/uii.dart
+++ lib/uii.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'clss/comm.dart';
+
+import 'model/comm.dart';
 
 class Api extends StatefulWidget {
   const Api({super.key});
